123d83ab3f41a1280021bd4e9738518511ce4159
AK <ak@argumenti.ru>
2013-02-21 12:22:09 +0400

    add mail & create dir log

--- write_log.php
+++ write_log.php
@@ -1,12 +1,22 @@
 <?php
 
-function write_log($message, $file = '') {
+function write_log($message, $mail = '', $file = '') {
 	
 	if (defined('LOGFILE'))
 		$file = LOGFILE;
 
+	$dir = dirname($file);
+	
+	if (!is_dir($dir))
+		if (!mkdir($dir, 0775, True))
+			return False;
+	
 	$value = '['.date('d.m.y\ H:i:s').'] '.$message.chr(13);		
 
-	file_put_contents($file, $value); #saved message
+	file_put_contents($file, $value); # saved message
+
+	if ($mail !== '')  # send mail 
+		mail($mail, "log ".$mail, $value); 
+	
 	
 }
